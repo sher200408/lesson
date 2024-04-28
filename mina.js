a4cf7064f89b24298781b74329aa9b9f39df39da
sher200408 <sherxansher61@gmail.com>
2024-04-28 23:16:32 +0500

    first commit

---
+++ mina.js
@@ -0,0 +1,205 @@
+"use strict"
+
+// let number = 5.1;//DAte Type  = Number
+
+// console.log(8/0);//Infinity
+
+// console.log("samar" * 8);//NaN
+
+// //string
+
+// const nimaClin = "samilbskhs jnciksnlka cjbkac jkaoikl"
+
+// // Boolean
+
+// const isMarried = true;
+// const isCile = false;
+
+// // ture = ha | tog'ri 
+// // false = yoq | notog'ri
+
+
+// // console.log(asdada)
+
+
+// let ul;
+// console.log(ul)
+
+
+// // object
+
+// const thief = {
+// //  key value 
+//     age: 30,
+//     jacket: "black",
+//     islong: false,
+
+// }
+// console.log(thief, " bu odam  ");
+
+
+// // array   uka si dilardi
+
+// const colors = ["red", "black" , "green , 10, [meni , kechir ], {salom}, "];
+
+// console.log(colors[5] );
+// confirm nima ture false;
+
+
+// const place = confirm("are you here?");
+// console.log(place);
+
+
+// alert("hello world ");
+
+// const currserClon = prompt("When kot you", "");
+// console.log(currserClon)
+
+
+// const currserClon = prompt("When kot you", "");
+// console.log(typeof currserClon)//sting boladi hammsai so'zmi yoki raqam boldim 
+
+// const age = +prompt("How old are you" , "");
+// console.log(typeof  age  ); // "18" + 10 = 1810;
+
+////////////////////////////////////
+// const faverter = [];
+// faverter[0] = " shermuhmmad ";
+// console.log(faverter);
+//////////////////////////////////////
+
+/////////////???????
+// typeof srting bosh yoki number bosh object qatardi 
+/////////////????
+
+//////////////////////////////////////////massiblar
+// const faverter = [];
+
+// faverter[0] = prompt("what's you faverot color #1", "");
+// faverter[1] = prompt("what's you faverot color #2", "");
+// faverter[2] = prompt("what's you faverot color #3", "");
+
+// console.log(faverter);
+////////////////////////////////////////////////massibler
+
+////////////////////////////////////////////////
+
+// const chenner = prompt("what you  favourte youtube");     1chi qisimda 
+// console.log("https://youtube.com/" + chenner + "/videos");
+
+///////////////////////////////////////////////
+
+// const chenner = prompt("what you  favourte youtube"); 
+// console.log(`https://youtube.com/${chenner}/videos`);
+
+
+// const user = "samar";
+// const job = "javascript developer";
+
+// console.log(`user name is ${user}, he is ${job}`);
+
+// const chennel = prompt("what's you jobs yoube");
+// console.log(`https//youtube.com/${chennel}`);
+//////////////////////////////////////////////////////////
+// const user = "samair";
+// const job  = "java is developer";
+
+// console.log(`user name is ${user} he is ${job}`)
+///////////////////////////////////////////////////////////
+
+// const client = prompt("Hello");
+// console.log(client);
+//////////////////////////////////////////////////
+
+///////////////opiradir//////////////////////////
+
+// console.log(6 + +"10");
+
+////////////////////////////////////////pirvix/////pirtex
+
+// incr, decr
+
+// let incr = 10;
+// let decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr,"",decr ,"" )
+
+
+////////////
+
+// console.log(10 % 4);
+
+// console.log(5 * 5 == 25);
+
+// console.log(5 * 5 === "25");
+
+//&& => va  || = yoki
+
+// const isAge = true;
+// const isClose = true;
+//                                 //true boldi kimat bir shir bolagn
+// console.log(isAge && isClose);
+
+// const isAge = true;
+// const isClose = true;
+// const chenkit= false;
+
+// console.log(isAge && isClose && !chenkit);
+
+// console.log(2 + 2 * 2 !== "6")
+
+// console.log(2 + 2 * 2 !== 6 );
+
+// let box = 10;
+// let boxs = 10;
+
+// console.log(++box);
+// console.log(++boxs);
+
+// let box = 10;
+// let boxs = 10;
+
+// console.log(box++);
+// console.log(boxs--);
+
+// console.log(box);
+// console.log(boxs);
+
+
+// console.log(5 % 3);
+
+
+// const numberOfsrivs = +prompt("Nechta serilar ko'rdiz","");
+
+// const seriesDB = {
+//     count: numberOfsrivs, 
+//     seriver: { },
+//     actors: {},
+//     genres: [],
+//     privet: false,
+// };
+
+// const a = prompt("oxirgi ko'rgan serialingiz"),
+//      b = prompt("nechi boxo bersiz"),
+//      c = prompt("Oxirgi ko'rgan serilingiz"),
+//      d = prompt("nechi baxo berasnsiz");
+
+//      seriesDB.seriver[a] = b;
+//      seriesDB.seriver[c] = d;
+
+
+    //  const theif  = {
+    //     jactek: "black",
+    //  }
+    //  const property =  "age" ,
+    //  value = 30;
+    //  theif[property] = value; 
+    //  console.log(theif);
+
+
+// console.log(seriesDB)
+
+
